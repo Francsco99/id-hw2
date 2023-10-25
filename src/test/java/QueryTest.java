a687034a608945cfd8a4ecdb83d6b371486a0220
Francesco <francesco.spezzano@outlook.com>
2023-10-25 22:07:49 +0200

    test sui titolo

--- src/test/java/QueryTest.java
+++ src/test/java/QueryTest.java
@@ -31,7 +31,7 @@ public class QueryTest {
     public void end() throws Exception{
         indexDirectory.close();
     }
-    @Test
+    @Test // Match di un termine su tutti i documenti
     public void termQueryAllDocs() throws Exception{
         TermQuery query = new TermQuery(new Term("contenuto","luogo"));
         TopDocs hits = searcher.search(query,10);
@@ -39,7 +39,7 @@ public class QueryTest {
         assertEquals(DirectoryReader.open(indexDirectory).maxDoc(),hits.scoreDocs.length);
     }
 
-    @Test
+    @Test // Match di un termine su un singolo documento
     public void termQuerySingleDoc() throws Exception{
         TermQuery query = new TermQuery(new Term("contenuto","italia"));
         TopDocs hits = searcher.search(query,10);
@@ -50,7 +50,7 @@ public class QueryTest {
         assertEquals("L'italia",match.get("titolo"));
     }
 
-    @Test
+    @Test // Match di un termine su nessun documento
     public void termQueryNoDoc() throws Exception{
         TermQuery query = new TermQuery(new Term("contenuto","pipistrello"));
         TopDocs hits = searcher.search(query,10);
@@ -58,7 +58,7 @@ public class QueryTest {
         assertEquals(0,hits.scoreDocs.length);
     }
 
-    @Test
+    @Test // Match di una phrase su un documento
     public void phraseQuerySingleDoc() throws Exception{
         PhraseQuery query = new PhraseQuery.Builder()
                 .add(new Term("contenuto","una"))
@@ -74,7 +74,7 @@ public class QueryTest {
         assertEquals("Città di notte",match.get("titolo"));
     }
 
-    @Test
+    @Test // Match di una phrase su due documenti
     public void phraseQueryMultipleDocs() throws Exception{
         PhraseQuery query = new PhraseQuery.Builder()
                 .add(new Term("contenuto","punti"))
@@ -91,4 +91,64 @@ public class QueryTest {
         assertEquals("Atmosfera di magia",secondMatch.get("titolo"));
     }
 
+    @Test // Match di un termine nel titolo di un singolo documento
+    public void termQueryTitleSingleDoc() throws IOException {
+        TermQuery query = new TermQuery(new Term("titolo","magia"));
+        TopDocs hits = searcher.search(query,10);
+
+        assertEquals(1,hits.scoreDocs.length);
+        Document match = searcher.doc(hits.scoreDocs[0].doc);
+
+        assertEquals("Atmosfera di magia",match.get("titolo"));
+
+    }
+
+    @Test // Match di un termine nel titolo di due documenti
+    public void termQueryTitleMultipleDoc() throws Exception{
+        TermQuery query = new TermQuery(new Term("titolo","di"));
+        TopDocs hits = searcher.search(query,10);
+
+        assertEquals(2,hits.scoreDocs.length);
+        Document firstMatch = searcher.doc(hits.scoreDocs[0].doc);
+        Document secondMatch = searcher.doc(hits.scoreDocs[1].doc);
+
+        assertEquals("Città di notte",firstMatch.get("titolo"));
+        assertEquals("Atmosfera di magia",secondMatch.get("titolo"));
+
+    }
+
+    @Test // Match di un termine nel titolo di nessun documento
+    public void termQueryTitleNoDoc() throws Exception{
+        TermQuery query = new TermQuery(new Term("titolo","scala"));
+        TopDocs hits = searcher.search(query,10);
+
+        assertEquals(0,hits.scoreDocs.length);
+    }
+
+    @Test // Match di una phrase nel titolo di un documento
+    public void phraseQueryTitleSingleDoc() throws Exception{
+        PhraseQuery query = new PhraseQuery.Builder()
+                .add(new Term("titolo","Notte"))
+                .add(new Term("titolo","silenziosa"))
+                .build();
+        TopDocs hits = searcher.search(query,10);
+        assertEquals(1,hits.scoreDocs.length);
+
+        Document match = searcher.doc(hits.scoreDocs[0].doc);
+
+        assertEquals("Notte silenziosa",match.get("titolo"));
+
+    }
+
+    @Test // Match di una phrase nel titolo di nessun documento
+    public void phraseQueryTitleNoDoc() throws Exception{
+        PhraseQuery query = new PhraseQuery.Builder()
+                .add(new Term("titolo","notte"))
+                .add(new Term("titolo","Silenziosa"))
+                .build();
+        TopDocs hits = searcher.search(query,10);
+
+        assertEquals(0,hits.scoreDocs.length);
+
+    }
 }
